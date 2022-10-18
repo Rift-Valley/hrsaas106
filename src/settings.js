ce1a0b3dcb093cb8b19715d71e9c2e44bbe256b2
ifhc <2689339948@qq.com>
2022-10-18 20:42:19 +0800

    人资项目初始化

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  // 项目标题
+  title: 'Vue Admin Template',
+
+  // 固定头部
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  // 是否显示侧边栏logo
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
